27113330521af92e70cb718235fa9acb88513eeb
LuisMi01 <lmurbezv@gmail.com>
2023-03-25 12:09:15 +0100

    Empezamos ejercicio 5

---
+++ src/main/java/Ejercicio5/Euclides.java
@@ -0,0 +1,12 @@
+package Ejercicio5;
+
+import java.util.Scanner;
+
+public class Euclides {
+    Scanner teclado = new Scanner(System.in);
+    int a, b, r;
+    public void algoritmo(){
+        System.out.println("Ingrese el primer numero:");
+        a = teclado.nextInt();
+    }
+}
