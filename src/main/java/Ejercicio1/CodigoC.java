a45df3e06719a744c06aa0cf408341f9cfbf523f
LuisMi01 <lmurbezv@gmail.com>
2023-03-25 10:55:28 +0100

    Ejercicio 1 termiando

---
+++ src/main/java/Ejercicio1/CodigoC.java
@@ -0,0 +1,26 @@
+package Ejercicio1;
+
+public class CodigoC {
+        double N = 2;
+        double PREC = 1e-6;
+
+        public double bisect(double min, double max){
+            double med = (min+max)/2;
+            if (max-min<PREC){
+                System.out.println(med);
+                return med;
+            }else if (f(min)*f(med)<0){
+                System.out.println(bisect(min,med));
+                return bisect(min,med);
+            }else{
+                System.out.println(bisect(med,max));
+                return bisect(med,max);
+            }
+
+        }
+        public double f(double x){
+            double N = 2;
+            double PREC = 1e-6;
+            return x*x-N;
+        }
+}
---
+++ src/main/java/Ejercicio1/Main.java
@@ -0,0 +1,8 @@
+package Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        CodigoC c = new CodigoC();
+        c.bisect(3.56, 2.90);
+    }
+}
