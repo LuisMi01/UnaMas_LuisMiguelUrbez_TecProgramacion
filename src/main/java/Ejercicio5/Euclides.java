97b030e386d42bd4c2ae1ea1d620f83a224cf0eb
LuisMi01 <lmurbezv@gmail.com>
2023-03-25 12:12:38 +0100

    Ejercicio 5 terminado

--- src/main/java/Ejercicio5/Euclides.java
+++ src/main/java/Ejercicio5/Euclides.java
@@ -5,8 +5,33 @@ import java.util.Scanner;
 public class Euclides {
     Scanner teclado = new Scanner(System.in);
     int a, b, r;
-    public void algoritmo(){
+    public void algoritmo1(){
+        System.out.println("FORMA RECURSIVA");
         System.out.println("Ingrese el primer numero:");
         a = teclado.nextInt();
+        System.out.println("Ingrese el segundo numero:");
+        b = teclado.nextInt();
+        while(b != 0){
+            r = a%b;
+            a = b;
+            b = r;
+        }
+        System.out.println("El minimo comun multiplo es: " + a);
+
     }
+    public void algoritmo2(){
+        System.out.println("FORMA ITERATIVA CON EXPRESIONES LAMBDA");
+        System.out.println("Ingrese el primer numero:");
+        a = teclado.nextInt();
+        System.out.println("Ingrese el segundo numero:");
+        b = teclado.nextInt();
+
+        while(b != 0){
+            r = a%b;
+            a = b;
+            b = r;
+        }
+        System.out.println("El minimo comun multiplo es: " + a);
+    }
+
 }
--- src/main/java/Ejercicio5/Main.java
+++ src/main/java/Ejercicio5/Main.java
@@ -2,6 +2,8 @@ package Ejercicio5;
 
 public class Main {
     public static void main(String[] args) {
-
+        Euclides euclides = new Euclides();
+        euclides.algoritmo1();
+        euclides.algoritmo2();
     }
 }
