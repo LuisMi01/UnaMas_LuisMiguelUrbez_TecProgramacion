9a5968946a31b00d8457bcda51528c653d52e70f
LuisMi01 <lmurbezv@gmail.com>
2023-03-25 11:57:54 +0100

    Ejercicio 3 terminado

---
+++ src/main/java/Ejericio2/Algoritmo.java
@@ -0,0 +1,39 @@
+package Ejericio2;
+
+public class Algoritmo {
+    public void f(int num /*100*/, int div/*2*/){
+        if (num>1) {
+            if ((num%div) == 0) {
+                System.out.println(div);
+                f(num/div,div);
+            } else {
+                f(num,div+1); }
+        } else {
+            System.out.println("El numero tiene que ser mayor que 1");
+        }
+    }
+
+    public void fIterativo(int num, int div){
+        while (num>1) {
+            if ((num%div) == 0) {
+                System.out.println(div);
+                num = num/div;
+            } else {
+                div++;
+            }
+        }
+        System.out.println("El numero tiene que ser mayor que 1");
+    }
+
+    public void fLambda(int num, int div){
+        while (num>1) {
+            if ((num%div) == 0) {
+                System.out.println(div);
+                num = num/div;
+            } else {
+                div++;
+            }
+        }
+        System.out.println("El numero tiene que ser mayor que 1");
+    }
+}
---
+++ src/main/java/Ejericio2/Main.java
@@ -0,0 +1,10 @@
+package Ejericio2;
+
+public class Main {
+    public static void main(String[] args) {
+        Algoritmo a = new Algoritmo();
+        a.f(100,2);
+        a.fIterativo(100,2);
+        a.fLambda(100,2);
+    }
+}
